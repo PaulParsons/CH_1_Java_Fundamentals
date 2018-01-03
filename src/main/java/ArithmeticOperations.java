c212d8d4f26dd72d9882bfffe3450b9f9f15cec5
Unknown <paul.c.parsons@gmail.com>
2018-01-03 15:00:28 -0700

    Java Chapter 1 Assignments

--- src/main/java/ArithmeticOperations.java
+++ src/main/java/ArithmeticOperations.java
@@ -1,11 +1,22 @@
 public class ArithmeticOperations {
 
 	// Write a static method that adds num1 to num2
+	public static int add(int num1, int num2){
+		return (num1 + num2);
+	}
 
 	// Write a static method that subtracts num2 from num1
+	public static int subtract(int num1, int num2){
+		return(num1-num2);
+	}
 
 	// Write a static method that multiplies num1 by num2
+	public static double multiply(double num1, double num2){
+		return(num1*num2);
+	}
 
 	// Write a static method that divides the numerator by the denominator
-
+public static int divide(int numerator, int denominator){
+		return(numerator/denominator);
+}
 }
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,4 +3,8 @@ public class HelloWorld {
 	// Write a static method that prints hello world to the console
 	// Your method should be called pirntHelloWorld
 
+	public static void printHelloWorld () {
+		System.out.print("Hello World");
+	}
+
 }
